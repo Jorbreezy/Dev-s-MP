c8458cb5d7f905ff1a1c41d58066913421b99b98
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-03-13 17:14:22 -0400

    Updates

--- server/models/model.js
+++ server/models/model.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const PG_URI = "postgres://smabkvvq:Ne2hS2gi8ux0ykfPK0SDrHniW8_Ci2A5@drona.db.elephantsql.com:5432/smabkvvq";
 
 const pool = new Pool({
-    connectionString: dbURI
+    connectionString: PG_URI
 });
 
 
